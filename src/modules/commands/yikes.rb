40297e609e33e82996fbd953c5146389d77df65e
Kraig Taylor <tay15028@byui.edu>
2019-08-23 20:41:44 -0600

    The Yikes command now checks for the Yikelord role

--- src/modules/commands/yikes.rb
+++ src/modules/commands/yikes.rb
@@ -1,10 +1,15 @@
 module Bot::DiscordCommands
-  # Adds an arbitrary amount of yikes to a user's yikes count
-  # This is the command that makes the bot
+  # Adds an arbitrary amount of yikes to a user's yike count
+  # This command can only be used by the Yikelord
   module Yikes
     extend Discordrb::Commands::CommandContainer
-    command :yikes do |_event|
-      '+1 yike!'
+    command :yikes do |event, username, amount = 1|
+      x = event.author.roles.find { |role| role.name == 'Yikelord' }
+      if x.nil?
+        event.channel.send_temporary_message("You are not worthy. I can only be commanded by the all powerful Yikelord.", 15)
+        break
+      end
+      event.channel.send_message(username + ' got ' + amount + ' yikes!')
     end
   end
 end
