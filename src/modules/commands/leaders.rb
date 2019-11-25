ea3a282a9e8d7f7718b292662ab37f496198f7b6
Kraig Taylor <tay15028@byui.edu>
2019-11-24 22:03:11 -0700

    Added message for leaders command

--- src/modules/commands/leaders.rb
+++ src/modules/commands/leaders.rb
@@ -4,7 +4,7 @@ module Bot::DiscordCommands
     module Leaders
       extend Discordrb::Commands::CommandContainer
       command :yikes do |event|
-        # TODO
+        event.channel.send_message("'username_woke' is the most woke, and 'username_yikes' is the most problematic.")
       end
     end
   end
