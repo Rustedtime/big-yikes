5a0eecc299f70446aeea4610ad6c70b3797bb5c3
Kraig Taylor <tay15028@byui.edu>
2019-12-10 20:06:29 -0700

    Changed leaders to show database.

--- src/modules/commands/leaders.rb
+++ src/modules/commands/leaders.rb
@@ -2,8 +2,9 @@ module Bot::DiscordCommands
     # Sends a message that reveals who is most woke in the server and who is most problematic
     module Leaders
       extend Discordrb::Commands::CommandContainer
-      command :yikes do |event|
-        event.channel.send_message("'username_woke' is the most woke, and 'username_yikes' is the most problematic.")
+      command :leaders do |event|
+        Bot::Database::Users.each { |user| event.channel.send_message("#{user[:user_id]} has #{user[:yikes]} yikes!")  }
+        #event.channel.send_message("'username_woke' is the most woke, and 'username_yikes' is the most problematic.")
       end
     end
   end
